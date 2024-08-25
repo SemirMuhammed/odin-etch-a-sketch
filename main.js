797f26214e4d0819fd4a542c4367f59740788f78
SemirMuhammed <semirmustafa22@gmail.com>
2024-08-25 09:22:18 +0300

    Update: Made the site responsive to different devices

--- main.js
+++ main.js
@@ -1,14 +1,49 @@
 // Draw Canvas Grid
 
 const canvas = document.getElementById("canvas");
-const gridSize = document.getElementById("grid-size").value
 
-for (let i = 0; i < (gridSize ** 2); i++) {
-	const grid = document.createElement("div");
+function drawCanvas(newCanvas = false) {
+	if (!newCanvas) {
+		const gridTotal = document.querySelectorAll(".square-grid");
 
-	grid.classList.add("square-grid");
-	grid.style.width = `calc(100% / ${gridSize})`;
-	grid.style.height = grid.style.width;
-	canvas.appendChild(grid);
+		gridTotal.forEach((element) => {
+			element.remove();
+		})
+	}
+
+	const gridSize = document.getElementById("grid-size").value
+
+	if (gridSize > 100) return alert("Canvas < 100");
+
+	for (let i = 0; i < (gridSize ** 2); i++) {
+		const grid = document.createElement("div");
+
+		grid.classList.add("square-grid");
+		grid.style.width = `calc(100% / ${gridSize})`;
+		canvas.appendChild(grid);
+	}
 }
 
+drawCanvas(true);
+
+// Change Canvas Grid
+
+const changeGridBtn = document.getElementById("change-grid-btn");
+
+changeGridBtn.addEventListener("click", drawCanvas);
+
+// Draw on Canvas
+
+let isDrawing = false;
+
+canvas.addEventListener("mousedown", () => isDrawing = true)
+
+canvas.addEventListener("mousemove", (event) => {
+	if (!isDrawing) return;
+
+	event.target.classList.add("fill-color");
+	window.getSelection().removeAllRanges();
+})
+
+window.addEventListener("mouseup", () => isDrawing = false)
+window.addEventListener("contextmenu", (event) => event.preventDefault())
